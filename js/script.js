d41d7c6aafec68872ec3d48fe8708f26ec8fb237
lokmane yakhlaf <2114693@talnet.nl>
2019-11-05 11:28:26 +0100

    javascript

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function verander() {
+    //hide foto 1
+    document.getElementById("foto1").style.display= "none";
+    document.getElementById("foto3").style.display= "block";
+}
+
+function verander2() {
+    //hide foto 1
+    document.getElementById("foto3").style.display= "none";
+    document.getElementById("foto1").style.display= "block";
+}
