4bdf3d783768382d3ea74e75c814eda8272f2850
lokmane yakhlaf <2114693@talnet.nl>
2019-11-06 10:18:44 +0100

    een paar us gedaan

--- js/script.js
+++ js/script.js
@@ -9,3 +9,36 @@ function verander2() {
     document.getElementById("foto3").style.display= "none";
     document.getElementById("foto1").style.display= "block";
 }
+function verander3() {
+    document.getElementById("foto2").style.display= "none";
+    document.getElementById("foto4").style.display= "block";
+    
+}
+function verander4() {
+    document.getElementById("foto4").style.display= "none";
+    document.getElementById("foto5").style.display= "block";
+    
+}
+function verander5() {
+    document.getElementById("foto5").style.display= "none";
+    document.getElementById("foto2").style.display= "block";
+    
+}
+function myFunction(x) {
+    x.classList.toggle("fa-thumbs-down");
+}
+function verander6() {
+    document.getElementById("foto6").style.display= "none";
+    document.getElementById("foto7").style.display= "block";
+}
+function verander7() {
+    document.getElementById("foto7").style.display= "none";
+    document.getElementById("foto8").style.display= "block";
+}
+function verander8() {
+    document.getElementById("foto8").style.display= "none";
+    document.getElementById("foto6").style.display= "block";
+}
+
+
+
